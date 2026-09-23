c7b0f9054f7cba5deb63a664e71a439ed87e2aaa
Jairo Quispe <shady0590@gmail.com>
2026-09-23 02:39:55 -0500

    Grupo 7 - Pregunta 5 - Jairo Quispe

---
+++ app/src/main/java/pe/edu/cibertec/myapplication/Pregunta5Activity.kt
@@ -0,0 +1,47 @@
+package pe.edu.cibertec.myapplication
+
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import pe.edu.cibertec.myapplication.databinding.ActivityPregunta5Binding
+
+class Pregunta5Activity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var binding: ActivityPregunta5Binding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityPregunta5Binding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnCalcular.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+
+        val texto = binding.edtDecibelios.text.toString()
+
+        if (texto.isEmpty()) {
+            binding.txtResultado.text = "Ingrese un valor"
+            return
+        }
+
+        val decibelios = texto.toDouble()
+
+        if (decibelios <= 55) {
+
+            binding.txtResultado.text =
+                "Nivel sonoro conforme a la ordenanza."
+
+        } else {
+
+            val exceso = decibelios - 55
+
+            val multa = exceso * 50
+
+            binding.txtResultado.text =
+                "Exceso: $exceso dB\nMulta: S/. $multa"
+        }
+    }
+}
