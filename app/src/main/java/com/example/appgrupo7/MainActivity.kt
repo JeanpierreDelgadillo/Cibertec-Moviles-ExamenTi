9c57e6997e0d89630b3b6b750ca9eb2a063f9a61
Jairo Quispe <shady0590@gmail.com>
2026-09-23 01:31:17 -0500

    Grupo 7 - Pregunta 5 - Jairo quispe

---
+++ app/src/main/java/com/example/appgrupo7/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.appgrupo7
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        startActivity(
+            Intent(
+                this,
+                Pregunta5Activity::class.java
+            )
+        )
+
+        finish()
+    }
+}
